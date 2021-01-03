f8bb44d5196b994e1756da05ff949adb214ed145
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2021-01-02 22:59:41 -0600

    commit tree 180362bbcb149558a0f9c02faaff660ce95da042

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,28 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :string
+    
+    def initialize(string)
+        @string = string
+    end
+
+    def get_response_body
+        uri = URI.parse(@string)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+
+    JSON
+        programs = JSON.parse(self.get_response_body)
+        programs.collect do |program|
+            program
+        end
+    end
+end
